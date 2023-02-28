9ae494859614a7ff287aad5521086f4f26e6a85f
motongxue <393888412@qq.com>
2023-02-28 14:48:58 +0800

    添加模块README.md文件

--- apps/all/api.go
+++ apps/all/api.go
@@ -1,10 +1,12 @@
 package all
 
 import (
-	// 注册所有HTTP服务模块, 暴露给框架HTTP服务器加载
-	_ "github.com/motongxue/cmdb-g7/apps/book/api"
-	_ "github.com/motongxue/cmdb-g7/apps/host/api"
-	_ "github.com/motongxue/cmdb-g7/apps/resource/api"
-	_ "github.com/motongxue/cmdb-g7/apps/secret/api"
-	_ "github.com/motongxue/cmdb-g7/apps/task/api"
+    // 注册所有HTTP服务模块, 暴露给框架HTTP服务器加载
+    _ "github.com/motongxue/cmdb-g7/apps/book/api"
+    _ "github.com/motongxue/cmdb-g7/apps/host/api"
+    _ "github.com/motongxue/cmdb-g7/apps/resource/api"
+    _ "github.com/motongxue/cmdb-g7/apps/secret/api"
+    _ "github.com/motongxue/cmdb-g7/apps/task/api"
 )
+
+// TODO 待添加各个模块的readme、以及mcenter为v0.0.5版本！
